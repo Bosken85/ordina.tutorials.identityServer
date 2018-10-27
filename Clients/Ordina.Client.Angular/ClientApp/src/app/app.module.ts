de3d7dd5119e91f3bbb80ec7f6da0ad524b09a68
Kevin Bosteels <Kevin.Bosteels@ordina.be>
2018-10-27 17:52:36 +0200

    setup security

--- Clients/Ordina.Client.Angular/ClientApp/src/app/app.module.ts
+++ Clients/Ordina.Client.Angular/ClientApp/src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { OAuthModule } from 'angular-oauth2-oidc';
 import { AppComponent } from './app.component';
+import { AppRouterModule } from './app.routes';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://www.angular.at/api'],
+        sendAccessToken: true
+      }
+    }),
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
