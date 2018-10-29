ece905ccbcbde126b44b7645fb8881d5ddbcee5a
Kevin Bosteels <Kevin.Bosteels@ordina.be>
2018-10-29 22:42:17 +0100

    Implement delegation

--- Resources/Ordina.Api/Controllers/ValuesController.cs
+++ Resources/Ordina.Api/Controllers/ValuesController.cs
@@ -2,21 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Newtonsoft.Json;
+using Ordina.Client.MVC;
 
 namespace Ordina.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    [Authorize(Roles = "Employee")]
     public class ValuesController : ControllerBase
     {
+        private readonly IPrivateApiHttpClient _privateApiHttpClient;
+
+        public ValuesController(IPrivateApiHttpClient privateApiHttpClient)
+        {
+            _privateApiHttpClient = privateApiHttpClient;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<ActionResult<IEnumerable<string>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var client = await _privateApiHttpClient.GetClient();
+            var response = await client.GetAsync("values");
+
+            var values = new List<string>();
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Request Message Information:- \n\n" + response.RequestMessage + "\n");
+                Console.WriteLine("Response Message Header \n\n" + response.Content.Headers + "\n");
+                // Get the response
+                var customerJsonString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Your response data is: " + customerJsonString);
+
+                // Deserialise the data (include the Newtonsoft JSON Nuget package if you don't already have it)
+                var deserialized = JsonConvert.DeserializeObject<IEnumerable<string>>(customerJsonString);
+                // Do something with it
+                values.AddRange(deserialized);
+            }
+            return values;
         }
 
         // GET api/values/5
